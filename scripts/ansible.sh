fb674bb378ab59d1671d89e96cc8214fbe8ef0da
akrishnakumar-splunk <akrishnakumar@splunk.com>
2018-07-26 22:58:25 +0530

    role option fixed during packer build

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -eux
 
 # Install Ansible repository.
-apt -y update && apt-get -y upgrade
-apt -y install software-properties-common
+apt -y update
 apt-add-repository ppa:ansible/ansible
 
 # Install Ansible.
